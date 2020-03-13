608c0f65017d9c7f713a669d5a8c2bdbdc8cd341
vitcool@ukr.net <vitcool@ukr.net>
2020-03-13 08:09:46 +0200

    Password encryption login route is added

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable func-names */
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
@@ -31,6 +35,14 @@ const userSchema = new mongoose.Schema(
       trim: true,
       minlength: 6,
     },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
   },
   {
     timestamps: true,
@@ -42,9 +54,45 @@ userSchema.methods.toJSON = function () {
   const publicUser = user.toObject();
 
   delete publicUser.password;
+  delete publicUser.tokens;
 
   return publicUser;
 };
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (user) {
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new Error('Unable to login');
+    }
+
+    return user;
+  }
+  throw new Error('Unable to login');
+};
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -10,9 +10,23 @@ router.post('/users', async (req, res) => {
 
   try {
     await user.save();
-    res.status(201).send({ user });
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(500).send(e.message);
+  }
+});
+
+router.post('/users/login', async (req, res) => {
+  const { body } = req;
+  const { email, password } = body;
+
+  try {
+    const user = await User.findByCredentials(email, password);
+    const token = await user.generateAuthToken();
+    res.send({ token, user });
+  } catch (e) {
+    res.status(400).send(e.message);
   }
 });
 
