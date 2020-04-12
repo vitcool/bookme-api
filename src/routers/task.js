1f2599106af7de08fbbc04b46c1d71503a72ffe6
vitcool@ukr.net <vitcool@ukr.net>
2020-04-12 23:22:49 +0300

    Get task by id endpoint is added

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-console */
 /* eslint-disable no-underscore-dangle */
 const express = require('express');
@@ -89,6 +90,20 @@ router.get('/tasks', auth, async (req, res) => {
     res.status(200).send({ tasks, total });
   } catch (e) {
     res.status(500).send(e.message);
+  }
+});
+
+// need to add documentation
+router.get('/task/:id', auth, async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await Task.findOne({ _id });
+    if (!task) {
+      return res.status(404).send();
+    }
+    return res.send(task);
+  } catch (e) {
+    res.status(500).send(e);
   }
 });
 
